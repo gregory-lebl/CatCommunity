b6b962523a63bb793d9bd30693ee0aa99658e0f6
Gregory <gregory.leblond.dev@gmail.com>
2022-04-17 14:55:46 +0200

    feat/responsive

---
+++ assets/image-preview.js
@@ -0,0 +1,21 @@
+// // let modalTrigger = document.getElementsByClassName('modal-trigger')
+// // let image = document.getElementsByClassName('cat-picture')
+// // let author = document.getElementsByClassName('picture-author')
+// //
+// // let modal = document.getElementById('modal-preview')
+// // let modalImg = document.getElementById('modal-picture')
+// // let modalAuthor = document.getElementById('modal-picture-author')
+//
+// function showZoomImage(){
+//     return console.log("Je suis dans showZoomImage")
+// }
+//
+// // modalTrigger.addEventListener('click', () => {
+// //     let author = modalTrigger.attributes["data-author"].value
+// //     let imageSrc = modalTrigger.attributes["data-image-src"].value
+// //     let imageTitle = modalTrigger.attributes["data-title"].value
+// //
+// //     console.log(author)
+// //     console.log(imageSrc)
+// //     console.log(imageTitle)
+// // })
---
+++ src/DataFixtures/CatFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Image;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Cocur\Slugify\Slugify;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+class CatFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $slugify = new Slugify();
+        $user = new User();
+
+        $user->setEmail("fixture@test.fr");
+        $user->setFirstname("Pierre");
+        $user->setLastname("Fixture");
+        $password = $this->hasher->hashPassword($user,"azerty");
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        for ($i = 0; $i <= 100; $i++){
+            $title = "Chat numéro " . $i;
+            $image = new Image();
+            $image->setUser($user);
+            $image->setName("cat625bdfcf0df7c.jpg");
+            $image->setSlug($slugify->slugify($title));
+            $image->setTitle($title);
+
+            $manager->persist($image);
+        }
+
+        $manager->flush();
+    }
+
+}
